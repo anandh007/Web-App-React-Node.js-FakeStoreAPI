40dc3bce9568a9aa725e2732b51abb13fd88387c
anandh007 <97468395+anandh007@users.noreply.github.com>
2025-11-12 15:51:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce";
+    const uri = process.env.MONGO_URI 
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
